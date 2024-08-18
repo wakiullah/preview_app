964a53d9885f60ebc36ac22bdc4fe25f8e73777b
wakiullah <m.wakiullah1212@gmail.com>
2024-08-18 23:32:02 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi maiores, consectetur ea assumenda non labore, placeat quaerat voluptatem sed accusantium, sunt quos nobis! Officia nemo ab quos laudantium minima cum!</p>
+    </>
+  )
+}
+
+export default App
